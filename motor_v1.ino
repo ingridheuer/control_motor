62620ce6a9c68882c45106cb055d017cd55eec7d
Ingrid Heuer <61297025+ingridheuer@users.noreply.github.com>
2021-07-20 11:14:00 -0300

    Update motor_v1.ino

--- motor_v1.ino
+++ motor_v1.ino
@@ -4,30 +4,6 @@
    Labo 7
 
    Automatización y control del electroimán del laboratorio de bajas temperaturas
-
-   Qué hace:
-   Control por serial, botonera
-   Homing automático con microswitches
-   Movimiento continuo con botonera
-   Homing por botonera
-   Movimiento automático por serial
-   Config. de velocidad por serial
-   Interrupción por microswitches
-   Feedback de la posición (en pasos) respecto a un 0 (home)
-   Media vuelta automática buscando switches
-
-
-   Qué falta (a 20/7):
-   Interfaz con labview
-   Media vuelta automática sin switches
-   Mediciones configuradas por usuario (ver labview)
-   Probar todos los comandos de la botonera (me faltan cables)
-   Config. de aceleración por serial
-   Ajustar por reducción (labview?)
-   Ajustar por "juego" (labview?)
-   Feedback en ángulos (labview?)
-   Ajuste de velocidad y aceleración preseteados (se puede hacer desde aca, ver por labview)
-   Calibración (labview?)
 */
 
 //librería para control de steppers
