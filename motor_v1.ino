604e47e8bb1752c6d053cc37e06dda369f34c919
Ingrid Heuer <61297025+ingridheuer@users.noreply.github.com>
2021-08-30 11:34:30 -0300

    refactored

--- motor_v1.ino
+++ motor_v1.ino
@@ -1,75 +1,76 @@
+
 /* Automatización de motor V1
-   Ingrid Heuer, Marco Crivaro Nicolini
+   Ingrid Heuer
    Julio 2021
    Labo 7
 
    Automatización y control del electroimán del laboratorio de bajas temperaturas
 */
 
-#include <AccelStepper.h> //librería para control de steppers
 
-//Defino los pins que voy a usar y variables de posición
-//Nota: home_switch y end_switch tienen que ser pines 2 y 3 si o si,
+/*
+ * La ultima vez cambiaste bocha de cosas: pasaste todas las funciones con comand a un if else grande (es mejor)
+ * y hiciste una clase para los botones
+ * Ahora tenes que: 
+ * Las variables de comando e inputs no necesitan ser globales! Van a ser locales y usadas solo si entra un comando
+ * Entonces, las funciones llamadas por comandos no tienen que tener el "if .... do", sino que las vamos a llamar desde el if else
+ * grande de serialCommands
+ * Todo lo que sea compartido entre serial y botones deberia ser una función aparte que podemosllamar desde los dos
+ * Algunas funciones van a necesitar inputs y otras outputs para que todo esto funcione
+ * Quizas hayaq que agregar algun static
+ */
+
+#include <AccelStepper.h> //librería para control de steppers
+#include <PushButton.h> //mini lib que me arme para los botones
+
+//Configuración general
+
+//Nota: homeSwitch y endSwitch tienen que ser pines 2 y 3 si o si,
 //porque son los únicos disponibles para interrupciones
-const byte home_switch = 2,
-           //end_switch = 3,
+const byte homeSwitch = 2,
+           //endSwitch = 3,
            dir = 4,
            pulse = 5,
-           greenpin = 10,
-           redpin = 11;
+           greenPin = 10,
+           redPin = 11;
 
-long backlash; //el "juego" del motor al cambiar de dirección
-
-typedef struct {
-  const byte pin;
-  int currentState;
-  unsigned long lastDebounceTime;
-} pushButton;
-
-pushButton rightButton = {6, HIGH, 0};
-pushButton leftButton = {7, HIGH, 0};
-pushButton homeButton = {8, HIGH, 0};
-pushButton stopButton = {9, HIGH, 0};
-
-long debounceDelay = 1000;
-
-
-//Creamos el objeto stepper
+//Creamos el objeto stepper de la clase AccelStepper
 AccelStepper stepper(1, dir, pulse); // 1 -> driver de 2 cables
+
+//Creamos los objetos de la clase PushButton
+unsigned long debounceDelay = 50;
+
+PushButton rightButton(6, false, debounceDelay);
+PushButton leftButton(7, false, debounceDelay);
+PushButton stopButton(8, true, debounceDelay);
+PushButton homeButton(9, true, debounceDelay);
 
 //=====================================
 // Variables para movimiento automático
 //=====================================
 
 //Definimos los estados que puede tomar el programa cuando está en modo automático
-enum auto_states
+enum states
 {
-  AUTOMED_DISABLED, INIT, WAITING_START, TOSTART, TOEND, WAITING_STOP
+  autoDisabled, goingToStart, waitingAtStart, goingToFinish, waitingAtFinish
 };
-auto_states auto_state = AUTOMED_DISABLED;
-
-enum button_states
-{
-  BUTTONS_DISABLED, BUTTONS_ENABLED
-};
-button_states buttons_state = BUTTONS_ENABLED;
+states state = autoDisabled;
 
 long Start; //donde empieza el recorrido
-long Stop; //donde termina
-int current_loops; //vueltas que dio hasta ahora
+long Finish; //donde termina
+int current_loop; //vueltas que dio hasta ahora //podría ser static
 int total_loops; //las que tiene que dar
 int speed_ida;
 int speed_vuelta;
+long backlash; //el "juego" del motor al cambiar de dirección
 
 // Millis = tiempo en milisegundos desde que arranca el programa
 // Lo usamos para que el motor espere al final del recorrido antes de cambiar de dirección
 // Nota: unsigned long porque son valores grandes, siempre positivos y si hubiera overflow queremos que
 // se resetee a 0
 
-unsigned long currentMillis; //tiempo actual
 unsigned long startMillis; //tiempo al principío del recorrido
 unsigned long stopMillis; //tiempo al final
-const int wait = 1000;  //tiempo que tiene que esperar
 
 //===========================
 // Variables para leer Serial
@@ -93,25 +94,25 @@ bool newData = false;
 //=========================
 
 void ISRhome() {
-  if (!digitalRead(home_switch)) {
+  if (!digitalRead(homeSwitch)) {
     stepper.stop();
-    auto_state = AUTOMED_DISABLED;
-    digitalWrite(redpin, HIGH);
+    state = autoDisabled;
+    digitalWrite(redPin, HIGH);
   }
   else {
-    digitalWrite(redpin, LOW);
+    digitalWrite(redPin, LOW);
   }
 }
 
 /*
   void ISRend() {
-  if (!digitalRead(end_switch)) {
+  if (!digitalRead(endSwitch)) {
     stepper.stop();
-    auto_state = AUTOMED_DISABLED;
-    digitalWrite(redpin, HIGH);
+    state = autoDisabled;
+    digitalWrite(redPin, HIGH);
   }
   else {
-    digitalWrite(redpin, LOW);
+    digitalWrite(redPin, LOW);
   }
   }
 */
@@ -123,96 +124,42 @@ void setup() {
   stepper.setMaxSpeed(500);
   stepper.setAcceleration(200);
 
-  pinMode(home_switch, INPUT_PULLUP);
-  //pinMode(end_switch, INPUT_PULLUP);
+  pinMode(homeSwitch, INPUT_PULLUP);
+  //pinMode(endSwitch, INPUT_PULLUP);
+  pinMode(greenPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
 
-  pinMode(rightButton.pin, INPUT_PULLUP);
-  pinMode(leftButton.pin, INPUT_PULLUP);
-  pinMode(stopButton.pin, INPUT_PULLUP);
-  pinMode(homeButton.pin, INPUT_PULLUP);
-
-  pinMode(greenpin, OUTPUT);
-  pinMode(redpin, OUTPUT);
-
-  attachInterrupt(digitalPinToInterrupt(home_switch), ISRhome, CHANGE);
-  //attachInterrupt(digitalPinToInterrupt(end_switch), ISRend, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(homeSwitch), ISRhome, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(endSwitch), ISRend, CHANGE);
 }
 
 
 void loop() {
 
-  currentMillis = millis();
   //===== se ejecutan solo si le hablamos por serial
   readserial();
   if (newData == true) {
     strcpy(tempChars, receivedChars); //hacemos una copia temporal de lo que recibimos en tempChars para proteger los datos originales
     parseData();
     newData = false;
-    setpoint();
-    measure();
-    rehome();
-    setspeed();
-    setaccel();
-    set_start();
-    set_stop();
-    if (command == "backlash") {
-      Serial.println(backlash);
-    }
-    if (command == "speed_ida") {
-      speed_ida = integer_input;
-      Serial.print("Speed ida set at: ");
-      Serial.println(speed_ida);
-    }
-    if (command == "speed_vuelta") {
-      speed_vuelta = integer_input;
-      Serial.print("Speed vuelta set at: ");
-      Serial.println(speed_vuelta);
-    }
-
-    //Manda el motor al inicio de la medición y cambia el estado a init. Resetea el contador de vueltas
-    if (command == "automed") {
-      total_loops = integer_input;
-      current_loops = 0;
-      stepper.moveTo(Start);
-      auto_state = INIT;
-      Serial.println("Automed init");
-    }
-
-    if (command == "stop") {
-      Serial.println("Stopping");
-      stepper.stop();
-      auto_state = AUTOMED_DISABLED;
-    }
-
-    if (command == "state") {
-      Serial.print(buttons_state);
-      Serial.print(" ");
-      Serial.println(auto_state);
-    }
+    serialCommands(command, integer_input, long_input);
   }
 
   //============ se ejecuta en cada loop
-
-  rightButton.currentState = buttonStateDebounce(rightButton);
-  leftButton.currentState = buttonStateDebounce(leftButton);
-  homeButton.currentState = buttonStateDebounce(homeButton);
-  stopButton.currentState = buttonStateDebounce(stopButton);
-
-
 
   buttonCommands();
   automed();
   stepper.run();
 
   if (stepper.isRunning()) {
-    digitalWrite(greenpin, HIGH);
-    if (buttons_state == BUTTONS_ENABLED) {
-      buttons_state = BUTTONS_DISABLED;
-    }
+    digitalWrite(greenPin, HIGH);
+    rightButton.enabled = false;
+    leftButton.enabled = false;
   }
   else {
-    digitalWrite(greenpin, LOW);
-    buttons_state = BUTTONS_ENABLED;
+    digitalWrite(greenPin, LOW);
+    rightButton.enabled = true;
+    leftButton.enabled = true;
   }
 }
 
@@ -223,77 +170,71 @@ void loop() {
 void rehome() {
   if ((command == "rehome")) {
     Serial.println("Homing");
-    digitalWrite(greenpin, HIGH);
+    digitalWrite(greenPin, HIGH);
 
-    while (digitalRead(home_switch)) {
+    while (digitalRead(homeSwitch)) {
       stepper.move(-1);
       stepper.run();
     }
 
     stepper.setCurrentPosition(0); //para medir el backlash
 
-    while (!digitalRead(home_switch)) {
+    while (!digitalRead(homeSwitch)) {
       stepper.move(1);
       stepper.run();
     }
 
     backlash = stepper.currentPosition(); //cuantos pasos dió desde que pisó el switch hasta que lo soltó
     stepper.setCurrentPosition(0);
-    digitalWrite(greenpin, LOW);
+    digitalWrite(greenPin, LOW);
   }
 }
 
 void automed() {
-  switch (auto_state) {
 
-    case AUTOMED_DISABLED:
+  switch (state) {
+
+    case autoDisabled:
       break;
 
-    case INIT:
+    case goingToStart:
       if (stepper.distanceToGo() == 0) {
-        Serial.println("Cambio a WAITING_START");
-        startMillis = currentMillis;
-        auto_state = WAITING_START;
+        startMillis = millis();
+        current_loop++;
+        if (current_loop > total_loops) {
+          Serial.println("Termine las vueltas, cambio a DISABLED");
+          state = autoDisabled;
+        }
+        else {
+          Serial.println("Faltan vueltas, cambio a WAITING AT START");
+          state = waitingAtStart;
+        }
       }
       break;
 
-    case WAITING_START:
-      if (currentMillis - startMillis >= wait) {
+    case waitingAtStart:
+      if (millis() - startMillis > 1000) {
         Serial.println("Cambio a TOEND");
-        stepper.moveTo(Stop);
+        stepper.moveTo(Finish);
         stepper.setMaxSpeed(speed_ida);
-        auto_state = TOEND;
+        state = goingToFinish;
       }
       break;
 
-    case TOEND:
+    case goingToFinish:
       if (stepper.distanceToGo() == 0) {
         Serial.println("Cambio a WAITING_STOP");
-        stopMillis = currentMillis;
-        auto_state = WAITING_STOP;
+        stopMillis = millis();
+        state = waitingAtFinish;
       }
       break;
 
-    case WAITING_STOP:
-      if (currentMillis - stopMillis >= wait) {
+    case waitingAtFinish:
+      if (millis() - stopMillis > 1000) {
         Serial.println("Cambio a TOSTART");
         stepper.moveTo(Start);
         stepper.setMaxSpeed(speed_vuelta);
-        auto_state = TOSTART;
-      }
-      break;
-
-    case TOSTART:
-      if (stepper.distanceToGo() == 0) {
-        current_loops++;
-        if (current_loops == total_loops) {
-          Serial.println("Termine las vueltas, cambio a DISABLED");
-          auto_state = AUTOMED_DISABLED;
-        }
-        else {
-          Serial.println("Faltan vueltas, cambio a INIT");
-          auto_state = INIT;
-        }
+        state = goingToStart;
       }
       break;
   }
@@ -304,101 +245,96 @@ void automed() {
 //====================================
 
 
-void measure() {
+void serialCommands(String command, int integer_input, long long_input) {
   if (command == "measure") {
     Serial.println(stepper.currentPosition());
   }
-}
-
-void setpoint() {
-  if (command == "setpoint") {
-    stepper.moveTo(long_input);
+  else if (command == "stop") {
+    Serial.println("Stopping");
+    stepper.stop();
+    state = autoDisabled;
+  }
+  else if (command == "setpoint") {
     Serial.print("Setpoint: ");
     Serial.println(long_input);
+    stepper.moveTo(long_input);
   }
-}
-
-void setspeed() {
-  if (command == "setspeed") {
-    stepper.setMaxSpeed(integer_input);
+  else if (command == "setspeed") {
     Serial.print("Speed set: ");
     Serial.println(integer_input);
+    stepper.setMaxSpeed(integer_input);
   }
-}
-
-void setaccel() {
-  if (command == "setaccel") {
-    stepper.setAcceleration(integer_input);
-    Serial.print("Accel set: ");
+  else if (command == "setaccel") {
+    Serial.print("Acccel set: ");
     Serial.println(integer_input);
+    stepper.setAcceleration(integer_input);
   }
-}
-
-void set_start() {
-  if (command == "setstart") {
-    Start = long_input;
+  else if (command == "setstart") {
     Serial.print("Start set at: ");
     Serial.println(Start);
+    Start = long_input;
+  }
+  else if (command == "setfinish") {
+    Serial.print("Finish set at: ");
+    Serial.println(Finish);
+    Finish = long_input;
+  }
+  else if (command == "automed") {
+    Serial.println("Automed initialized");
+    total_loops = integer_input;
+    current_loop = 0;
+    stepper.moveTo(Start);
+    state = goingToStart;
+  }
+  else if (command == "state") {
+    Serial.println(state);
+  }
+  else if (command == "backlash") {
+    Serial.println(backlash);
+  }
+  else if (command == "speed_ida") {
+    speed_ida = integer_input;
+    Serial.print("Speed ida set at: ");
+    Serial.println(speed_ida);
+  }
+  else if (command == "speed_vuelta") {
+    speed_vuelta = integer_input;
+    Serial.print("Speed vuelta set at: ");
+    Serial.println(speed_vuelta);
+  }
+  else {
+    Serial.println("Invalid command");
   }
 }
 
-void set_stop() {
-  if (command == "setstop") {
-    Stop = long_input;
-    Serial.print("Stop set at: ");
-    Serial.println(Stop);
-  }
-}
+
 
 //=============
 // Botones
 //=============
 
-int buttonStateDebounce(pushButton button) {
-  int reading = digitalRead(button.pin);
-
-  if ((reading != button.currentState) && (millis() - button.lastDebounceTime >= debounceDelay)) {
-    button.lastDebounceTime = millis();
-    button.currentState = reading;
-
-    if (reading == LOW) {
-      Serial.println("Pressed");
-    }
-    if (reading == HIGH) {
-      Serial.println("Released");
-    }
-  }
-  return button.currentState;
-}
-
-
-
 void buttonCommands() {
-  if (!stopButton.currentState) {
+  if (stopButton.isOn()) {
     Serial.println("Stop button pressed");
     stepper.stop();
-    auto_state = AUTOMED_DISABLED;
-    //stopButton.toggled = 0;
   }
 
-  if (!homeButton.currentState) {
+  if (homeButton.isOn()) {
     Serial.println("Home button pressed");
     rehome();
-    //homeButton.toggled = 0;
   }
 
+  if (rightButton.enabled && rightButton.isOn()) {
+    Serial.println("+1");
+    stepper.move(1);
+  }
 
-  if (buttons_state == BUTTONS_ENABLED) {
-    if (!rightButton.currentState) {
-      Serial.println("+1");
-      stepper.move(1);
-    }
-    if (!leftButton.currentState) {
-      Serial.println("-1");
-      stepper.move(-1);
-    }
+  if (leftButton.enabled && leftButton.isOn()) {
+    Serial.println("-1");
+    stepper.move(-1);
   }
 }
+
 
 //=========================
 // Comunicación por Serial
