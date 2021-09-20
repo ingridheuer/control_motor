0f4b9c64089eca1c1730cfaf6627be6768e5c1be
Ingrid Heuer <61297025+ingridheuer@users.noreply.github.com>
2021-09-19 21:13:40 -0300

    acorte comandos

--- motor_v1.ino
+++ motor_v1.ino
@@ -7,15 +7,6 @@
    Automatización y control del electroimán del laboratorio de bajas temperaturas
 */
 
-
-/*
-   La ultima vez cambiaste bocha de cosas: pasaste todas las funciones con comand a un if else grande (es mejor)
-   y hiciste una clase para los botones
-   Ahora tenes que:
-   Todo lo que sea compartido entre serial y botones deberia ser una función aparte que podemosllamar desde los dos
-   Algunas funciones van a necesitar inputs y otras outputs para que todo esto funcione
-*/
-
 #include <AccelStepper.h> //librería para control de steppers
 #include <PushButton.h> //mini lib que me arme para los botones
 
@@ -33,6 +24,7 @@ const byte homeSwitch = 2,
 
 const long debounceDelay = 50;
 bool buttonsEnabled = true;
+long baudRate = 115200;
 
 AccelStepper stepper(1, dirPin, pulsePin); // 1 -> driver de 2 cables
 
@@ -48,17 +40,21 @@ PushButton homeButton(homePin, true, debounceDelay);
 //Estados que puede tomar el programa cuando está en modo automático
 enum states
 {
-  autoDisabled, goingToStart, waitingAtStart, goingToFinish, waitingAtFinish
+  autoDisabled, goingToStart, waitingStart, goingToFinish, waitingFinish
 };
 states state = autoDisabled;
 
-long Start;
-long Finish;
-int total_loops;
-int speed_ida;
-int speed_vuelta;
-long backlash;
+typedef struct {
+  long start;
+  long finish;
+  int total_loops;
+  int speed_ida;
+  int speed_vuelta;
+} Recorrido;
 
+Recorrido recorrido;
+
+long backlash;
 const long wait = 1000;
 //===========================
 // Variables para leer Serial
@@ -110,7 +106,7 @@ void ISRhome() {
 //======================
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(baudRate);
 
   stepper.setMaxSpeed(100);
   stepper.setAcceleration(200);
@@ -146,7 +142,7 @@ void loop() {
   }
   else {
     digitalWrite(greenPin, LOW);
-    if ((state == autoDisabled) && !buttonsEnabled){
+    if ((state == autoDisabled) && !buttonsEnabled) {
       //Si los deshabilité y terminó de moverse, los vuelvo a prender
       buttonsEnabled = true;
     }
@@ -193,40 +189,40 @@ void automed() {
       if (stepper.distanceToGo() == 0) {
         startMillis = millis();
         current_loop++;
-        if (current_loop > total_loops) {
-          Serial.println("Termine las vueltas, cambio a DISABLED");
+        if (current_loop > recorrido.total_loops) {
+          //Serial.println("Termine las vueltas, cambio a disabled y reseteo loops");
           state = autoDisabled;
           current_loop = 0;
         }
         else {
-          Serial.println("Faltan vueltas, cambio a WAITING AT START");
-          state = waitingAtStart;
+          //Serial.println("Cambio a waiting start");
+          state = waitingStart;
         }
       }
       break;
 
-    case waitingAtStart:
+    case waitingStart:
       if (millis() - startMillis > wait) {
-        Serial.println("Cambio a TOEND");
-        stepper.moveTo(Finish);
-        stepper.setMaxSpeed(speed_ida);
+        //Serial.println("Cambio a going to finish");
+        stepper.moveTo(recorrido.finish);
+        stepper.setMaxSpeed(recorrido.speed_ida);
         state = goingToFinish;
       }
       break;
 
     case goingToFinish:
       if (stepper.distanceToGo() == 0) {
-        Serial.println("Cambio a WAITING_STOP");
+        //Serial.println("Cambio a waiting finish");
         stopMillis = millis();
-        state = waitingAtFinish;
+        state = waitingFinish;
       }
       break;
 
-    case waitingAtFinish:
+    case waitingFinish:
       if (millis() - stopMillis > wait) {
-        Serial.println("Cambio a TOSTART");
-        stepper.moveTo(Start);
-        stepper.setMaxSpeed(speed_vuelta);
+        //Serial.println("Cambio a going to stsart");
+        stepper.moveTo(recorrido.start);
+        stepper.setMaxSpeed(recorrido.speed_vuelta);
         state = goingToStart;
       }
       break;
@@ -237,70 +233,63 @@ void automed() {
 // Funciones para comandos de LabVIEW
 //====================================
 
-
 void serialCommands(Message message) {
-  if (message.command == "measure") {
+  if (message.command == "MES") {
     Serial.println(stepper.currentPosition());
   }
-  else if (message.command == "stop") {
-    Serial.println("Stopping");
+  else if (message.command == "STP") {
+    Serial.println("STP");
     stepper.stop();
     state = autoDisabled;
     buttonsEnabled = false;
   }
-  else if (message.command == "setpoint") {
-    Serial.print("Setpoint: ");
-    Serial.println(message.long_input);
+  else if (message.command == "SET") {
+    Serial.println("SET");
     buttonsEnabled = false;
-    rightButton.enabled = false;
-    leftButton.enabled = false;
     stepper.setMaxSpeed(message.integer_input);
     stepper.moveTo(message.long_input);
   }
-  else if (message.command == "setaccel") {
-    Serial.print("Acccel set: ");
-    Serial.println(message.integer_input);
+  else if (message.command == "ACC") {
+    Serial.println("ACC");
     stepper.setAcceleration(message.integer_input);
   }
-  else if (message.command == "vuelta") {
-    Serial.print("Start set at: ");
-    Serial.println(Start);
-    Start = message.long_input;
-    speed_vuelta = message.integer_input;
+  else if (message.command == "VUE") {
+    Serial.println("VUE");
+    recorrido.start = message.long_input;
+    recorrido.speed_vuelta = message.integer_input;
   }
-  else if (message.command == "ida") {
-    Serial.print("Finish set at: ");
-    Serial.println(Finish);
-    Finish = message.long_input;
-    speed_ida = message.integer_input;
+  else if (message.command == "IDA") {
+    Serial.println("IDA");
+    recorrido.finish = message.long_input;
+    recorrido.speed_ida = message.integer_input;
   }
-  else if (message.command == "automed") {
-    Serial.println("Automed initialized");
-    total_loops = message.integer_input;
-    stepper.moveTo(Start);
+  else if (message.command == "AUT") {
+    Serial.println("AUT");
+    recorrido.total_loops = message.integer_input;
+    stepper.moveTo(recorrido.start);
     buttonsEnabled = false;
     state = goingToStart;
   }
-  else if (message.command == "info") {
-    Serial.print("auto state: ");
-    Serial.print(state);
-    Serial.print(" ");
-    Serial.print("buttons state: ");
-    Serial.println(buttonsEnabled);
-    Serial.print("left button: ");
-    Serial.println(leftButton.enabled);
-    Serial.print("right button: ");
-    Serial.println(rightButton.enabled);
-  }
-  else if (message.command == "backlash") {
+  else if (message.command == "BCK") {
     Serial.println(backlash);
   }
-  else if (message.command = "rehome") {
-    Serial.println("homing");
+  else if (message.command = "HOM") {
+    Serial.println("HOM");
     rehome();
   }
+  else if (message.command == "INF") {
+    Serial.print("Auto State: ");
+    Serial.println(state);
+    Serial.print("Buttons State: ");
+    Serial.println(buttonsEnabled);
+    Serial.print("BAUD rate: ");
+    Serial.println(baudRate);
+  }
+  else if (message.command == "IDN"){
+    Serial.println("Ingrid Heuer, Agosto 2021");
+  }
   else {
-    Serial.println("Invalid command");
+    Serial.println("NAK");
   }
 }
 
@@ -312,21 +301,21 @@ void serialCommands(Message message) {
 
 void buttonCommands() {
   if (stopButton.isOn()) {
-    Serial.println("Stop button pressed");
+    //Serial.println("Stop button pressed");
     stepper.stop();
     state = autoDisabled;
     buttonsEnabled = false;
   }
   else if (buttonsEnabled && rightButton.isOn()) {
-    Serial.println("+1");
+    //Serial.println("+1");
     stepper.move(1);
   }
   else if (buttonsEnabled && leftButton.isOn()) {
-    Serial.println("-1");
+    //Serial.println("-1");
     stepper.move(-1);
   }
   else if (homeButton.isOn()) {
-    Serial.println("Home button pressed");
+    //Serial.println("Home button pressed");
     rehome();
   }
 }
